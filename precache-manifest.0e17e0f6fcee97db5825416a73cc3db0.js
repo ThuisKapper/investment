7f86eb6b1037677ba7982c9cf2620181decc39d9
Bernardus Molkenboer <bernardusmolkenboer@MacBook-Pro-van-Bernardus.local>
2021-08-09 11:50:05 +0200

    Updates

---
+++ precache-manifest.0e17e0f6fcee97db5825416a73cc3db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a491315defe71ad6e563",
+    "url": "/static/css/main.fab296b0.chunk.css"
+  },
+  {
+    "revision": "a491315defe71ad6e563",
+    "url": "/static/js/main.a491315d.chunk.js"
+  },
+  {
+    "revision": "b23d0484669e66889a9b",
+    "url": "/static/css/1.d2f289a1.chunk.css"
+  },
+  {
+    "revision": "b23d0484669e66889a9b",
+    "url": "/static/js/1.b23d0484.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f421e68e39c792f61f75c011fe39c510",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.daeb5cb023ee107984b1550249ac4f46.js"
+  "/precache-manifest.0e17e0f6fcee97db5825416a73cc3db0.js"
 );
 
 workbox.clientsClaim();
